9919a5ea5074310fdc2dc91628e69b25cb4b15eb
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2025-01-16 22:14:07 +0100

    Create BluetoothTrapPrank-SD.ino

---
+++ BluetoothTrapPrank-SD.ino
@@ -0,0 +1,5 @@
+// !DO NOT USE FOR ILLEGAL ACTIVITIES !
+// need sd card where will be prank video to start AUTOMATICALLY after connecting to esp32 (fake speaker)
+// ALSO it will set MAX VOLUME on mobile phone speaker !
+
+// CODE
